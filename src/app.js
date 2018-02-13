c6a7054ad0c5bd624bfff8e174caa94d3ffdf448
Amal Nazeem <amalnazeem@gmail.com>
2018-02-12 21:05:53 -0500

    Making a react app without create-react-app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-console.log("Hello");
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+// import anime from 'animejs';
+//
+//
+// anime({
+//   targets: document.getElementById('hello-text'),
+//   translateX: [
+//     { value: 100, duration: 1200 },
+//     { value: 0, duration: 800 }
+//   ],
+//   rotate: '1turn',
+//   backgroundColor: '#FFF',
+//   duration: 2000,
+//   loop: true
+// });
+
+ReactDOM.render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebPackPlugin = require('html-webpack-plugin');
 
-var BUILD_DIR = path.resolve(__dirname, 'dist/');
-var APP_DIR = path.resolve(__dirname, 'src/');
+var APP_DIR = path.resolve(__dirname, 'dist');
+var SRC_DIR = path.resolve(__dirname, 'src');
 
 var config = {
-  entry: APP_DIR + 'app.js',
+  entry: SRC_DIR + '/app.js',
   output: {
-    path: BUILD_DIR,
+    path: APP_DIR,
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    loaders: [
+      {
+        loader:'babel-loader',
+        test: /\.js?$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    contentBase: APP_DIR
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: SRC_DIR + '/index.html',
+      filename: APP_DIR + '/index.html'
+    })
+  ]
 }
 
 module.exports = config;
