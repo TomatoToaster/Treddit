512309a9a104a2ca1c7427e9acf687377b3896fc
Amal Nazeem <amalnazeem@gmail.com>
2018-02-27 09:51:26 -0500

    cleaned up webpack.config a little bit

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
-var webpack = require('webpack');
-var path = require('path');
-var HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-var APP_DIR = path.resolve(__dirname, 'dist');
-var SRC_DIR = path.resolve(__dirname, 'src');
+const APP_DIR = path.resolve(__dirname, 'dist');
+const SRC_DIR = path.resolve(__dirname, 'src');
 
-var config = {
-  entry: SRC_DIR + '/app.js',
+const config = {
+  entry: `${SRC_DIR}/app.js`,
   output: {
     path: APP_DIR,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       {
         test: /\.js?$/,
-        use:'babel-loader',
-        exclude: /node_modules/
+        use: 'babel-loader',
+        exclude: /node_modules/,
       },
       {
-        test:/\.(s*)css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        exclude: /node_modules/,
+      },
+    ],
   },
   devServer: {
-    contentBase: APP_DIR
+    contentBase: APP_DIR,
   },
   plugins: [
     new HtmlWebPackPlugin({
       hash: true,
       title: 'Free of Create-React-App at Last',
-      template: SRC_DIR + '/index.html',
-      filename: APP_DIR + '/index.html'
-    })
-  ]
-}
+      template: `${SRC_DIR}/index.html`,
+      filename: `${APP_DIR}/index.html`,
+    }),
+  ],
+};
 
 module.exports = config;
